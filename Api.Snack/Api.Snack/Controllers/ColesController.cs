92903a0ccc5828e65bfd74187c0bd9d87577012a
EnzyCue <mohammadalhakim787@gmail.com>
2023-07-02 16:37:10 +1000

    Add coles url

--- Api.Snack/Api.Snack/Controllers/ColesController.cs
+++ Api.Snack/Api.Snack/Controllers/ColesController.cs
@@ -9,6 +9,8 @@ namespace Api.Snack.Controllers
     {
         private readonly ILogger<ColesController> _logger;
 
+        private readonly string _colesProductEndpoint = "https://www.coles.com.au/product/";
+
         public ColesController(ILogger<ColesController> logger)
         {
             _logger = logger;
@@ -17,10 +19,13 @@ namespace Api.Snack.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetProductById(string id)
         {
+            string url = $"{_colesProductEndpoint}{id}";
             return new OkObjectResult(id);
         }
 
         // go from id -> html url
+
+
         // use html agility pack to extract the information about the product
         // encode the information into an object
         // send the object back to the user through api
