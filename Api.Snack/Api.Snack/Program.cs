292b17eb4c61687d357a61d697f1ab36171ab5df
EnzyCue <mohammadalhakim787@gmail.com>
2023-07-15 21:26:46 +1000

    Make new cache service object

--- Api.Snack/Api.Snack/Program.cs
+++ Api.Snack/Api.Snack/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<ColesService>();
 builder.Services.AddSingleton<WoolworthsService>();
+builder.Services.AddSingleton<CacheService>();
 
 var app = builder.Build();
 
---
+++ Api.Snack/Api.Snack/Services/CacheService.cs
@@ -0,0 +1,34 @@
+﻿namespace Api.Snack.Services
+{
+    public class CacheService
+    {
+        public Dictionary<string, object> Cache { get; set; }
+        public CacheService()
+        {
+            Cache = new Dictionary<string, object>();
+        }
+
+        public void SetCacheKey(string key, object value)
+        {
+            Cache[key] = value;
+        }
+
+        public void RemoveCacheKey(string key)
+        {
+            if (Cache.ContainsKey(key))
+            {
+                Cache.Remove(key);
+            }
+        }
+
+        public T? GetCache<T>(string key)
+        {
+            if (!Cache.TryGetValue(key, out object? value))
+                return default;
+
+            if (value is not T) return default;
+
+            return (T)value;
+        }
+    }
+}
