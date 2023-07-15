ee648a65a5900153c98d530c99a81496b0f9b30e
EnzyCue <mohammadalhakim787@gmail.com>
2023-07-15 19:29:38 +1000

    add selenium implementation for woolworths service

--- Api.Snack/Api.Snack/Services/WoolworthsService.cs
+++ Api.Snack/Api.Snack/Services/WoolworthsService.cs
@@ -1,6 +1,8 @@
 ﻿using Api.Snack.Models;
-using HtmlAgilityPack;
 using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 
 namespace Api.Snack.Services
 {
@@ -8,34 +10,42 @@ namespace Api.Snack.Services
     {
         private readonly string _woolworthsProductEndpoint = "https://www.woolworths.com.au/apis/ui/product/detail/";
         private readonly string _user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
+        private readonly string _cookie1 = "_abck";
+        private readonly string _cookie2 = "bm_sz";
         /*        private readonly string _woolworthsProductEndpoint = "https://www.woolworths.com.au/shop/productdetails/";*/
 
         public async Task GetWoolworthsProduct(Product product)
         {
             // go from id -> html url
             string url = $"{_woolworthsProductEndpoint}{product.Id}";
-
-            // use html agility pack to extract the information about the product
-            /*            var web = new HtmlWeb();
-                        var htmlDoc = web.Load(url);*/
-
-            // ---- Scrape data ----
-
-            //Scrape Image
-            /*            var image = htmlDoc.DocumentNode.SelectSingleNode("//img[@class='main-image-v2 u-noOutline']");*/
+            var cookies = GetCookies(url);
 
             // create http client and make request
-            HttpWebRequest req = new HttpWebRequest(url);
-            req.Method = "GET";
-            var resp2 = req.GetResponse();
-
-            WebRequest myReq = WebRequest.Create(url);
-
-            var resp = myReq.GetResponse();
-
             var client = new HttpClient();
             var message = new HttpRequestMessage(new HttpMethod("GET"), url);
             message.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
+        }
+
+        private (string, string) GetCookies(string url)
+        {
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                // Navigate to Url
+                driver.Navigate().GoToUrl(url);
+
+                // Get cookie details with named cookie 'foo'
+                var cookie1 = driver.Manage().Cookies.GetCookieNamed(_cookie1);
+                var cookie2 = driver.Manage().Cookies.GetCookieNamed(_cookie2);
+
+                return (cookie1.Value, cookie2.Value);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+
+            throw new Exception("Failed to fetch cookies");
         }
     }
 }
