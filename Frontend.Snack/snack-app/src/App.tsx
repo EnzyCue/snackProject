132dbd8ed22075d26cda636b709008fb3aed0674
EnzyCue <mohammadalhakim787@gmail.com>
2023-07-23 16:01:54 +1000

    Setup initial landing page for website

--- Frontend.Snack/snack-app/src/App.tsx
+++ Frontend.Snack/snack-app/src/App.tsx
@@ -1,16 +1,25 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-   <>
-    <div className='flex flex-col content-start justify-end'>
-      <div> test1 </div>
-      <div> test 2 </div>
-      <div> test 3 </div>
-    </div>
-   </>
-  )
+    <>
+      {/* Root background */}
+      <div className="flex flex-row h-full bg-color">
+        {/* First left half of screen */}
+        <div className="flex flex-col justify-center grow">
+          {/* Content info */}
+          <div className="bg-cyan-300">
+            <h1>Snack App</h1>
+            <div>hello2</div>
+          </div>
+        </div>
+        {/* First Right half of screen */}
+        <div className="flex flex-col justify-center grow">
+          <div>hello</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
