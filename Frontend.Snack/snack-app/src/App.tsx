d856e2464ace3896eef4752cc9cd75c5ec6e361c
EnzyCue <mohammadalhakim787@gmail.com>
2023-07-23 16:47:18 +1000

    Setup breakpoints + Add title and description

--- Frontend.Snack/snack-app/src/App.tsx
+++ Frontend.Snack/snack-app/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
@@ -8,13 +9,20 @@ function App() {
         {/* First left half of screen */}
         <div className="flex flex-col justify-center grow">
           {/* Content info */}
-          <div className="bg-cyan-300">
-            <h1>Snack App</h1>
-            <div>hello2</div>
+          <div className="ml-0 md:ml-[15%]">
+            <h1 className="text-gray-900 text-5xl md:text-6xl text-4xl font-bold leading-none tracking-tight">
+              Snack App
+            </h1>
+            <p className="text-lg text-gray-900 md:text-xl font-normal md:max-w-xl my-4 md:my-6">
+              Effortlessly compare snack prices between stores with our app.
+              Find the best deals, save money, and navigate your snack shopping
+              with real-time updates and an easy-to-use interface. Your smart,
+              cost-effective snack shopping companion.
+            </p>
           </div>
         </div>
         {/* First Right half of screen */}
-        <div className="flex flex-col justify-center grow">
+        <div className="flex flex-col justify-center grow hidden xl:block">
           <div>hello</div>
         </div>
       </div>
