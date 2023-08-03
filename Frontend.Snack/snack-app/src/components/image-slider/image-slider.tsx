b9c48e3d7e2740b5d4a40253b2568a2187a4c762
EnzyCue <mohammadalhakim787@gmail.com>
2023-08-03 22:39:46 +1000

    debug carousel

--- Frontend.Snack/snack-app/src/components/image-slider/image-slider.tsx
+++ Frontend.Snack/snack-app/src/components/image-slider/image-slider.tsx
@@ -1,39 +1,40 @@
 import ImageGallery from "react-image-gallery";
 import "./image-slider.css";
+import styles from "./image-slider.module.css";
 
 export function ImageSlider() {
   const images = [
     {
       original: "https://picsum.photos/id/1018/1000/600/",
       thumbnail: "https://picsum.photos/id/1018/250/150/",
-      sizes: 100,
     },
     {
       original: "https://picsum.photos/id/1015/1000/600/",
       thumbnail: "https://picsum.photos/id/1015/250/150/",
-      originalHeight: 100,
     },
     {
       original: "https://picsum.photos/id/1019/1000/600/",
       thumbnail: "https://picsum.photos/id/1019/250/150/",
-      originalHeight: 100,
     },
   ];
 
   return (
-    <div className="w-full h-1/4">
-      <ImageGallery
-        items={images}
-        useBrowserFullscreen={false}
-        useTranslate3D={false}
-        autoPlay={true}
-        disableKeyDown={true}
-        showBullets={true}
-        showThumbnails={false}
-        showPlayButton={false}
-        showNav={false}
-        showFullscreenButton={false}
-      />
+    <div className="h-full flex justify-start items-center">
+      <div className="w-[43em] h-[26em]">
+        <ImageGallery
+          items={images}
+          useBrowserFullscreen={false}
+          useTranslate3D={false}
+          autoPlay={true}
+          disableKeyDown={true}
+          showBullets={true}
+          showThumbnails={false}
+          showPlayButton={false}
+          showNav={false}
+          showFullscreenButton={false}
+          additionalClass={styles.image}
+        />
+      </div>
     </div>
   );
 }
