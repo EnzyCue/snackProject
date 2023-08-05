8d5fba549d8f2be9ca688bbaa18588ec58460b70
EnzyCue <mohammadalhakim787@gmail.com>
2023-08-05 15:45:06 +1000

    add the other carousel images

--- Frontend.Snack/snack-app/src/components/image-slider/image-slider.tsx
+++ Frontend.Snack/snack-app/src/components/image-slider/image-slider.tsx
@@ -8,12 +8,10 @@ export function ImageSlider() {
       original: "/image-1.png",
     },
     {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
+      original: "/image-3.png",
     },
     {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
+      original: "/image-2.png",
     },
   ];
 
